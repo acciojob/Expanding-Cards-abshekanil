c6bde358954d71251aa635784b05982c14deba7a
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-06 16:08:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function(){
+	var panels = document.querySelectorAll('.panel');
+
+	panels.forEach(function(panel){
+		panel.addEventListener('click', function(){
+			panels.forEach(function(p){
+				p.classList.remove('active');
+			});
+			panel.classList.add('active');
+		});
+	});
+});
